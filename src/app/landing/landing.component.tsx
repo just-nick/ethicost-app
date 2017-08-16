a7852aa2a1780dc76a1305818586ef958a45f5de
just-nick <dev@motome.com.au>
2017-08-16 14:48:56 +1000

    change a to Link

--- src/app/landing/landing.component.tsx
+++ src/app/landing/landing.component.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 
 class LandingComponent extends React.Component<any, any> {
     private AUTH_CLIENT_ID: string = 'aqMchxLQ3sldvvKEZxMBmd95lBhnhm4z';
@@ -15,7 +16,7 @@ class LandingComponent extends React.Component<any, any> {
             <div>
                 <h1>Welcome to Ethicost</h1>
                 <p>This is content</p>
-                <a href="/score">Get score (Temporary)</a>
+                <Link to="/score">Get score (Temporary)</Link>
                 <a href={this.AUTH_URL}>Login</a>
             </div>
         );
