1bed1dda9b172c09310d9fed8aada9aa3c44892a
Moto Me <support@motome.com.au>
2017-08-16 12:47:00 +1000

    Adds login button and redirect to Macquarie login

--- src/app/application.tsx
+++ src/app/application.tsx
@@ -7,6 +7,10 @@ import LandingComponent from './landing/landing.component';
 // import SaintChooserComponent from './app/saint/saint-chooser/saint-chooser.component';
 
 class Application extends React.Component<ProviderProps & DispatchProp<any>, {}> {
+    private AUTH_CLIENT_ID: string = 'aqMchxLQ3sldvvKEZxMBmd95lBhnhm4z';
+    private AUTH_STATE: string = 'NSW';
+    private AUTH_URL: string = `https://sandbox.api.macquariebank.io/connect/v1/user-interface/login?client_id=${this.AUTH_CLIENT_ID}&state=${this.AUTH_STATE}`;
+
     constructor(props: ProviderProps & DispatchProp<any>) {
         super(props);
         //this.props.dispatch(SaintActions.find());
@@ -15,7 +19,10 @@ class Application extends React.Component<ProviderProps & DispatchProp<any>, {}>
     render() {
         return (
             <div className="ethicost">
-                <header>EthiCost</header>
+                <header>
+                    <h1>EthiCost</h1>
+                    <a href={this.AUTH_URL}>Login</a>
+                </header>
                 <div className="body">
                     <Provider store={this.props.store}>
                         <BrowserRouter>
