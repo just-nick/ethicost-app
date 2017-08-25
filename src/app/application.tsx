f1ee60f3f41a30d5c962981f852df532e5cf4e33
Phil <philip.lavis@macquarie.com>
2017-08-25 10:29:59 +1000

    Logo now goes home

--- src/app/application.tsx
+++ src/app/application.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
-import {connect, DispatchProp, Provider, ProviderProps} from 'react-redux';
-import {BrowserRouter, Route} from 'react-router-dom';
-import AuthorizeComponent from './auth/authorize.component';
-import LandingComponent from './landing/landing.component';
-import ScoreCardComponent from './score/score-card.component';
+import * as React from "react";
+import {connect, DispatchProp, Provider, ProviderProps} from "react-redux";
+import {BrowserRouter, Route} from "react-router-dom";
+import AuthorizeComponent from "./auth/authorize.component";
+import LandingComponent from "./landing/landing.component";
+import ScoreCardComponent from "./score/score-card.component";
 
 class Application extends React.Component<ProviderProps & DispatchProp<any>, {}> {
     constructor(props: ProviderProps & DispatchProp<any>) {
@@ -14,14 +14,18 @@ class Application extends React.Component<ProviderProps & DispatchProp<any>, {}>
         return (
             <div className="ethicost">
                 <header>
-                    <h1>EthiCost</h1>
+                    <a href="/">
+                        <h1>
+                            EthiCost
+                        </h1>
+                    </a>
                 </header>
                 <div className="body">
                     <Provider store={this.props.store}>
                         <BrowserRouter>
                             <switch>
-                                <Route exact path="/" component={LandingComponent as any} />
-                                <Route exact path="/score" component={ScoreCardComponent as any} />
+                                <Route exact path="/" component={LandingComponent as any}/>
+                                <Route exact path="/score" component={ScoreCardComponent as any}/>
                                 <Route exact path="/authorizing" component={AuthorizeComponent as any}/>
                             </switch>
                         </BrowserRouter>
